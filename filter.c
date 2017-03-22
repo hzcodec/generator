f3dc76fcf7d9fa2510afe72a454d7d655fc710f0
heinz.samuelsson <heinz.samuelsson@unjo.com>
2017-03-22 18:09:53 +0100

    Now working

--- filter.c
+++ filter.c
@@ -13,12 +13,39 @@
 #include "frame1.h"
 #include "common.h"
 
+struct TriggerPoint {
+  	float maxValue;
+	int   idx;
+};
+
+//void Filter__find_max_filter_value(struct TriggerPoint *tp, float val, int n)
+//{
+// 	if (val > tp->maxValue)
+//	{
+//		tp->maxValue = val;
+//		tp->idx = n;
+//		printf("%s() - max:%.2f, idx:%d\n", __func__, tp->maxValue, tp->idx);
+//	}
+//}
+
+void Filter__find_max_input_value(struct TriggerPoint *tp, float val, int n)
+{
+// 	if (val > tp->maxValue)
+//	{
+//		tp->maxValue = val;
+//		tp->idx = n;
+//		printf("%s() - max:%.2f, idx:%d\n", __func__, tp->maxValue, tp->idx);
+//	}
+}
+
 void Filter__filter(float *ar, struct Generator *gen, char *s)
 {
         FILE *fp;
 
         float new = 0.0;
         float last = 0.0;
+	struct TriggerPoint inputTp;
+	struct TriggerPoint filterTp;
  
         fp = fopen(s, "w");
 
@@ -27,14 +54,18 @@ void Filter__filter(float *ar, struct Generator *gen, char *s)
         for(int n=0; n<gen->numberOfSamples; n++)
         {
 	        //printf("%s() - alpha: %.4f, ar[%d]: %.4f, new: %.4f\n", __func__, gen->alpha, n, ar[n], new);
+
+		Filter__find_max_input_value(&inputTp, ar[n], n);
                 new = ar[n];
                 new = last + gen->alpha*(new - last);
+		//Filter__find_max_filter_value(&filterTp, new, n);
 
                 fprintf(fp, "%.4f\n", new);
                 last = new;
         }
 
-	printf("'%s' created\n", s);
+        //float trigLevel = tp.maxValue * 0.75;
+	printf("'%s' created, Max input value: %.1f at index: %d, Max filter value: %.1f at index: %d\n", s, inputTp.maxValue, inputTp.idx, filterTp.maxValue, filterTp.idx);
 
 	fclose(fp);
 }
--- filter.h
+++ filter.h
@@ -2,5 +2,7 @@
 #define _FILTER_H_
 
 void Filter__filter(float *ar, struct Generator *gen, char *s);
+void Filter__find_max_input_value(struct TriggerPoint *tp, float val, int n);
+void Filter__find_max_filter_value(struct TriggerPoint *tp, float val, int n);
 
 #endif
