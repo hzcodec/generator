0658662265a41a8565617f71abdfabcc22b603bb
Heinz <heinz.samuelsson@gmail.com>
2017-03-24 23:58:43 +0100

    Mupp

---
+++ common.py
@@ -0,0 +1,11 @@
+NUMBER_OF_FIELDS = 11
+
+ALPHA_POS = 2
+
+SAMPLING_TIME = 83.0 # us
+
+font = {'family': 'monospace',
+        'color':  'black',
+        'weight': 'bold',
+        'size': 10,
+        }
